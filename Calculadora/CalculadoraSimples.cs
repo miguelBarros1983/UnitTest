8d674b5fcaf4fe5addc99bab505bb09bd6537bd5
José Miguel Barros da Silva <miguelbarros1983@hotmail.com>
2017-09-18 14:13:06 +0100

    My fisrt commit

---
+++ Calculadora/CalculadoraSimples.cs
@@ -0,0 +1,47 @@
+﻿//Calculadora Simples
+
+using System;
+
+namespace Testes_Unitarios
+{
+
+    public class CalculadoraSimples
+    {
+        public double Soma(double num1, double num2)
+        {
+            double res = num1 + num2;
+            if (res == 0)
+            {
+                throw new Exception("ERRO");
+            }
+            else
+            {
+                return num1 + num2;
+            }
+        }
+        public double Subtrai(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+        public double Divide(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+        public double Multiplica(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
+        public double RaizQuadrada(int number)
+        {
+            if (number == 0 )
+            {
+                throw new Exception("ERRO");
+            }
+            else
+            {
+                return Math.Sqrt(number);
+            }
+        }
+    }
+}
---
+++ CalculadoraTest/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Testes_Unitarios;
+
+namespace CalculadoraTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private const int A = 10;
+        private const int B = 10;
+        private const int C = 9;
+
+
+        [TestMethod]
+        // Nesta função o resultado esperado é uma exceção com uma mensagem de erro "ERRO"
+        [ExpectedException(typeof(Exception), "Erro porque resultado é igual a zero!")] 
+        public void TestMetodoSoma_IgualZero_ResultadoException()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+            double resultado = calculadora.Soma(0, 0);
+        }
+
+        [TestMethod]
+        [Timeout(50)] //Milliseconds - To set a timeout on an individual test method
+
+        public void TestMetodoSoma()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+            double resultado = calculadora.Soma(A, B);
+            Assert.AreEqual(resultado, 20);
+        }
+
+        [TestMethod]
+        [Timeout(TestTimeout.Infinite)] // Milliseconds - To set the timeout to the maximum allowed
+        public void TestMetodoSubtrai()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+            double resultado = calculadora.Subtrai(A, B);
+            Assert.AreEqual(resultado, 0);
+        }
+
+        [TestMethod]
+        public void TestMetodoDivide()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+            double resultado = calculadora.Divide(A, B);
+            Assert.AreEqual(resultado, 1);
+        }
+
+        [TestMethod]
+        public void TestMetodoMultiplica()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+            double resultado = calculadora.Multiplica(A, B);
+            Assert.AreEqual(resultado, 100);
+        }
+
+        [TestMethod]
+        public void RecebeInteiro_DevolveDouble_ResultadoCorreto()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+            double resultado = calculadora.RaizQuadrada(C);
+            Assert.AreEqual(resultado, 3);
+            //comment
+        }
+
+        [TestMethod]
+        // Nesta função o resultado esperado é uma exceção com uma mensagem de erro "ERRO"
+        [ExpectedException(typeof(Exception), "Erro porque resultado é igual a zero!")]
+        public void TestMetodoSRaizQuadrada_IgualZero_ResultadoException()
+        {
+            CalculadoraSimples calculadora = new CalculadoraSimples();
+            double resultado = calculadora.RaizQuadrada(0);
+        }
+    }
+}
